3e5be64817fed1685728e7a133bf1c1a78858e3f
Ronieee18 <ronitparwani78@gmail.com>
2024-04-17 17:48:43 +0530

    commited

--- src/routes/feedback.route.js
+++ src/routes/feedback.route.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { verifyToken } from "../utils/tokenManager.js";
 import { getallFeedbacks, postFeedback } from "../controllers/feedback.controller.js";
 const  feedbackRouter = Router();
-feedbackRouter.use(verifyToken);
+// feedbackRouter.use(verifyToken);
 feedbackRouter.get('/',getallFeedbacks);
-feedbackRouter.post('/',postFeedback);
+feedbackRouter.post(verifyToken,'/',postFeedback);
 export default feedbackRouter
