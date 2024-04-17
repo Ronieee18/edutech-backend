57a95175ee70582481720a30b4b105f21fd0a5ae
Ronieee18 <ronitparwani78@gmail.com>
2024-04-17 17:40:38 +0530

    commited

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ import router from './routes/index.js';
 
 const app=express();
 
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+// app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://edutechlearning.netlify.app",credentials:true}))
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan("dev"));
